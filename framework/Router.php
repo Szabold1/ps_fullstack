bd81aef774245e2a79a190b2abd1c11b62153e15
Boldizsar Szabo <szboldisz@gmail.com>
2025-01-14 09:15:35 +0100

    Add authentication checks to router

--- framework/Router.php
+++ framework/Router.php
@@ -9,17 +9,53 @@ class Router
 {
     private static $routes = [
         "POST" => [
-            "/register" => ["controller" => UserController::class, "method" => "register"],
-            "/login" => ["controller" => UserController::class, "method" => "login"],
-            "/logout" => ["controller" => UserController::class, "method" => "logout"],
-            "/profile/edit" => ["controller" => UserController::class, "method" => "editProfile"],
+            "/register" => [
+                "controller" => UserController::class,
+                "method" => "register",
+                "auth" => 'guest'
+            ],
+            "/login" => [
+                "controller" => UserController::class,
+                "method" => "login",
+                "auth" => 'guest'
+            ],
+            "/logout" => [
+                "controller" => UserController::class,
+                "method" => "logout",
+                "auth" => 'user'
+            ],
+            "/profile/edit" => [
+                "controller" => UserController::class,
+                "method" => "editProfile",
+                "auth" => 'user'
+            ],
         ],
         "GET" => [
-            "/" => ["controller" => UserController::class, "method" => "index"],
-            "/register" => ["controller" => UserController::class, "method" => "pageRegister"],
-            "/login" => ["controller" => UserController::class, "method" => "pageLogin"],
-            "/profile" => ["controller" => UserController::class, "method" => "pageProfile"],
-            "/profile/edit" => ["controller" => UserController::class, "method" => "pageEditProfile"],
+            "/" => [
+                "controller" => UserController::class,
+                "method" => "index",
+                "auth" => 'guest'
+            ],
+            "/register" => [
+                "controller" => UserController::class,
+                "method" => "pageRegister",
+                "auth" => 'guest'
+            ],
+            "/login" => [
+                "controller" => UserController::class,
+                "method" => "pageLogin",
+                "auth" => 'guest'
+            ],
+            "/profile" => [
+                "controller" => UserController::class,
+                "method" => "pageProfile",
+                "auth" => 'user'
+            ],
+            "/profile/edit" => [
+                "controller" => UserController::class,
+                "method" => "pageEditProfile",
+                "auth" => 'user'
+            ],
         ]
     ];
 
@@ -29,21 +65,32 @@ class Router
         $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
         $route = Router::$routes[$method][$path] ?? null;
-        // If the route is found, call the controller method
         if ($route) {
+            // check if the user is authorized to access the route
+            $auth = $route["auth"];
+            $isLoggedIn = Session::get('user');
+            if ($auth === 'guest' && $isLoggedIn) {
+                redirect('/profile');
+            }
+            if ($auth === 'user' && !$isLoggedIn) {
+                redirect('/');
+            }
+
+            // call the controller method if it exists
             $controller = new $route["controller"]();
             $controllerMethod = $route["method"];
-
             if (method_exists($controller, $controllerMethod)) {
                 $controller->$controllerMethod();
                 exit;
             }
+
+            // if the controller method does not exist, return an error
             http_response_code(Response::$INTERNAL_SERVER_ERROR);
             loadView('error', ['message' => 'Belső kiszolgálóhiba']);
             exit;
         }
 
-        // If the route is not found, return an error
+        // if the route is not found, return an error
         http_response_code(Response::$NOT_FOUND);
         loadView('error', ['message' => 'A keresett oldal nem található']);
         exit;
