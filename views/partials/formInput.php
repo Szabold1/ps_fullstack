038f5f8c024ee4453ece81e596e17b679a8fd280
Boldizsar Szabo <szboldisz@gmail.com>
2025-01-14 12:34:28 +0100

    Fix the use of required attribute

--- views/partials/formInput.php
+++ views/partials/formInput.php
@@ -4,7 +4,7 @@ $name ??= '';
 $value ??= '';
 $type ??= '';
 $placeholder ??= '';
-$required ??= false;
+$required ??= '';
 $error ??= '';
 ?>
 
@@ -16,9 +16,8 @@ $error ??= '';
         type="<?= $type ?>"
         name="<?= $name ?>"
         value="<?= $value ?? '' ?>"
-        id="email"
         autoComplete="<?= $name ?>"
-        required="<?= $required ?>"
+        <?= $required ? 'required' : '' ?>
         placeholder="<?= $placeholder ?>"
         class="block w-full rounded-md bg-white mt-2 px-3 py-1.5 text-base  outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
     <?php if ($error) : ?>
